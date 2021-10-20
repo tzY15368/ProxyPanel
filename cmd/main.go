2d897fc6ab2b120e4f71698bcc3f4357841975bb
NathanL <tzy15368@outlook.com>
2021-10-20 20:04:35 +0200

    add config parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/tzY15368/proxypanel/config"
+)
 
 func main() {
-	fmt.Println("")
+	cfg, err := config.InitConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	if cfg.Master.Enabled {
+
+	}
+	if cfg.Worker.Enabled {
+
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,50 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func InitConfig() (*Config, error) {
+	cfg := &Config{}
+	file, err := os.Open("config.json")
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+type Config struct {
+	HeartBeatRatePerSec int
+	HeartBeatErrorThres int
+	LogPath             string
+	Worker              *WorkerCFG
+	Master              *MasterCFG
+}
+
+type WorkerCFG struct {
+	Enabled    bool
+	NicName    string
+	WorkerHost string
+	WorkerPort int
+	MasterIP   string
+	MasterPort int
+}
+
+type MasterCFG struct {
+	Enabled bool
+	Db      string
+	Host    string
+	Port    int
+	RpcHost string
+	RpcPort int
+}
